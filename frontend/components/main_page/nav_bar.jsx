dfe3a3dbd0f478a15d6de7f481a861ed1d223c02
Kai Chen <kaifujidesu@gmail.com>
2018-03-15 14:14:21 -0400

    Fixed css maybe?

--- frontend/components/main_page/nav_bar.jsx
+++ frontend/components/main_page/nav_bar.jsx
@@ -12,45 +12,45 @@ class NavBar extends React.Component {
 
   render () {
     return (
-      <nav className="main-page-nav">
-          <h1 className='logo-and-search-bar'>
-            <button>
-              <Link to='/feed'>a</Link>
-            </button>
-            <AutoSuggestSearchContainer />
-          </h1>
-        <div className="main-page-nav-right-side">
-          <ul className="main-page-nav-ul">
-            <Link to={`/profile/${this.props.currentUser.id}`}>
-              <button className="main-page-nav-ul-button">
-                <span className="main-page-nav-ul-span">
-                  <img src={this.props.currentUser.image_url}></img>
-                </span>
-                {this.props.currentUser.first_name}
+        <nav className="main-page-nav">
+            <h1 className='logo-and-search-bar'>
+              <button>
+                <Link to='/feed'>a</Link>
               </button>
-            </Link>
-            <ul className="main-page-nav-ul-home">
-              <button className="main-page-nav-ul-button-home">
-                <Link to='/feed'>Home</Link>
-              </button>
-            </ul>
-          </ul>
-          <ul className="nav-bar-pics">
-            <NavBarDropdownMenuContainer users={this.props.users}/>
-            <li className="nav-bar-people">
-              <img src={window.message}></img>
-            </li>
-            <li className="nav-bar-people">
-              <img src={window.globe}></img>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-bar-main-button">
-              <button onClick={() => this.props.logout()}>Logout</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+              <AutoSuggestSearchContainer />
+            </h1>
+            <div className="main-page-nav-right-side">
+              <ul className="main-page-nav-ul">
+                <Link to={`/profile/${this.props.currentUser.id}`}>
+                  <button className="main-page-nav-ul-button">
+                    <span className="main-page-nav-ul-span">
+                      <img src={this.props.currentUser.image_url}></img>
+                    </span>
+                    {this.props.currentUser.first_name}
+                  </button>
+                </Link>
+                <ul className="main-page-nav-ul-home">
+                  <button className="main-page-nav-ul-button-home">
+                    <Link to='/feed'>Home</Link>
+                  </button>
+                </ul>
+              </ul>
+              <ul className="nav-bar-pics">
+                <NavBarDropdownMenuContainer users={this.props.users}/>
+                <li className="nav-bar-people">
+                  <img src={window.message}></img>
+                </li>
+                <li className="nav-bar-people">
+                  <img src={window.globe}></img>
+                </li>
+              </ul>
+              <ul>
+                <li className="nav-bar-main-button">
+                  <button onClick={() => this.props.logout()}>Logout</button>
+                </li>
+              </ul>
+            </div>
+        </nav>
     );
   }
 }
